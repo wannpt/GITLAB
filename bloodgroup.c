6d323156acbe19966325bfc4e69794aa84437db1
wannpt <naphatthorn.k@gmail.com>
2019-11-19 18:10:27 +0700

    I add madam in bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,7 +5,7 @@ void main()
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("What is your Name madam? ");
     scanf("%s", Name);
     do
     {
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("Goodbye madam\n");
 }
